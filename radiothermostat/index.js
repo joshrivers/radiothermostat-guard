070a1ad59e5265113def19dd772ef4cb0499ec63
Josh Rivers <josh@joshrivers.me>
2019-02-15 17:04:38 -0800

    Removing cruft and unused/uncovered files.

--- radiothermostat/index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports.discover = require('./discover');
-module.exports.get = require('./get');
-module.exports.post = require('./post');
---
+++ test/manageThermostat.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const { expect } = require('chai');
+const proxyquire = require('proxyquire').noCallThru();
+const sinon = require('sinon');
+
+describe('Thermostat auto policy enforcement launcher', () => {
+  it('should discover the radiothermostat IP address', (done) => {
+    const discoverStub = sinon.stub().resolves(['address']);
+    const autoHeatCoolStub = {mainLoop:sinon.stub()};
+    proxyquire('../manageThermostat.js', {
+      './src/autoHeatCool': autoHeatCoolStub
+    });
+
+    sinon.assert.calledOnce(autoHeatCoolStub);
+    expect(true).to.be.equal(true);
+    done();
+  });
+});
